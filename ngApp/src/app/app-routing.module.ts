478adb8926384afc4cfd9b3f15c20e165b32f95b
MaxGsprd <max.gaspard@outlook.fr>
2020-12-04 21:41:19 +0100

    isntall angular, bootstrap & add  basic html

---
+++ ngApp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './components/events/events.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SpecialEventsComponent } from './components/special-events/special-events.component';
+
+const routes: Routes = [
+  {path:'', redirectTo: '/events', pathMatch: 'full'},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'events', component: EventsComponent},
+  {path: 'special', component: SpecialEventsComponent},
+  { path: '**', redirectTo: '/events'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
